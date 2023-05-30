c3e811fcf6fa4273f83a3a0acdd160f0f19314f2
KJS <digyrh456789@gmail.com>
2023-05-30 13:25:47 +0900

    UPDATE :: 유저 네임과 서버 코드로 정보 불러오기 가능

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-const soap = require('soap');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-// Define the SOAP request payload
-const soapRequest = {
-    GetDataRequest: {
-        inputParameter: '123'
+let sname = '';
+const getCharacterInfo = async (characterName, server) => {
+    try {
+        // Make a GET request to the character page
+        const response = await axios.get(`https://maplestory.nexon.com/Ranking/World/Total?c=${characterName}&w=${server}`);
+        const html = response.data;
+        // Load the HTML into Cheerio
+        const $ = cheerio.load(html);
+        let servername = '';
+        // Extract character information
+        const characterInfo = {
+            characterprofile: $('.char_img', '.search_com_chk').find('img').attr('src'),
+            characterserverpicture: $('.search_com_chk').find('dt').find('img').attr('src'),
+            charactername: $('.left', '.search_com_chk').find('dd').text().substring(5, 10),
+            characterlevel: $('.search_com_chk').find('td:nth-child(3)').text(),
+            characterpopular: $('.search_com_chk').find('td:nth-child(5)').text(),
+            characterguild: $('.search_com_chk').find('td:nth-child(6)').text()
+        };
+        servername = characterInfo.characterserverpicture.substring(72, 73);
+        if (servername == '4') {
+            sname = '오로라';
+        }
+        // Return the character information
+        return characterInfo;
+    } catch (error) {
+        throw error;
     }
 };
 
-// Set the SOAP API endpoint URL
-const url = 'https://api.maplestory.nexon.com/soap/';
+// Example usage
+const characterName = '보람둘이'; // Replace with the character name you want to search
+const server = '3'; // Replace with the server name
 
-// Create a SOAP client
-soap.createClient(url, function (err, client) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    // Call the SOAP API operation
-    client.GetData(soapRequest, function (err, result) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        // Extract the output parameter from the SOAP response
-        const outputData = result.outputParameter;
-        console.log(outputData);
+getCharacterInfo(characterName, server)
+    .then((characterInfo) => {
+        console.log('Character Profile:', characterInfo.characterprofile);
+        console.log('Character Server:', sname);
+        console.log('Server Picture:', characterInfo.characterserverpicture);
+        console.log('Charactername:', characterInfo.charactername);
+        console.log('CharacterLevel:', characterInfo.characterlevel);
+        console.log('Characterpopular:', characterInfo.characterpopular);
+        console.log('Characterguild:', characterInfo.characterguild);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
     });
-});
